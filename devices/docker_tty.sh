de571ac6bf544a349a1fad86cbbfd3f1cfce4137
Iwan Vosloo <iwan@reahl.org>
2023-05-26 08:41:59 +0200

    First-cut with 2 inverters going and fixed ttyUSB links inside docker

--- devices/docker_tty.sh
+++ devices/docker_tty.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash  
-                                                           
-echo "Usb event: $1 $2 $3 $4" >> /var/log/docker_tty.log        
-if [ ! -z "$(docker ps -qf name=homeassistant)" ]                                     
-then                                                                            
-if [ "$1" == "added" ]                                                          
-    then                                                                        
-        docker exec -u 0 homeassistant mknod $2 c $3 $4                               
-        docker exec -u 0 homeassistant chmod -R 777 $2                                
-        docker exec -u 0 homeassistant ln -s $2 /dev/ttyUSB-SERIAL
-        echo "Adding $2 to docker" >> /var/log/docker_tty.log                
-    else                                                                        
-        docker exec -u 0 homeassistant rm $2                                          
-        docker exec -u 0 homeassistant rm /dev/ttyUSB-SERIAL
-        echo "Removing $2 from docker" >> /var/log/docker_tty.log            
-    fi                                                                          
-fi 
+#!/usr/bin/env bash
+
+echo "$(date): Usb event: $1 $2 $3 $4 $5" >> /var/log/docker_tty.log
+if [ ! -z "$(docker ps -qf name=homeassistant)" ]
+then
+if [ "$1" == "added" ]
+    then
+        docker exec -u 0 homeassistant mknod $2 c $3 $4
+        docker exec -u 0 homeassistant chmod -R 777 $2
+        docker exec -u 0 homeassistant ln -s $2 /dev/ttyINVERTER$5
+        echo "$(date): Adding $2 (inverter $5) to docker" >> /var/log/docker_tty.log
+    else
+        docker exec -u 0 homeassistant rm $2
+        docker exec -u 0 homeassistant rm /dev/ttyINVERTER$5
+        echo "$(date): Removing $2 (inverter $5) from docker" >> /var/log/docker_tty.log
+    fi
+fi
